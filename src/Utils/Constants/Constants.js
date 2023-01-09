fec3cc94d369a62ffb61f444b959720d482f5465
Ali Dbouk <60514754+DboukAli98@users.noreply.github.com>
2023-01-10 01:38:59 +0200

    api change

--- src/Utils/Constants/Constants.js
+++ src/Utils/Constants/Constants.js
@@ -1,2 +1,2 @@
-const base_url = "http://scoowebapp-env-1.eba-m4xsyb5i.eu-west-3.elasticbeanstalk.com";
+const base_url = "https://scoopaatapiproduction-env-1.eba-vvpnr5jr.eu-west-3.elasticbeanstalk.com";
 export default base_url;
